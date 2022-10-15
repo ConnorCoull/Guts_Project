59caf34e45f270d93dc65e876b8daf9687c0f531
Alicolliar <alicolliar@gmail.com>
2022-10-15 22:48:55 +0100

    Various CSS and Django beasts have been slaughtered!
    But, alas, many brave braincells were lost in the fight

--- gtrgtr/gtrgtrapp/views.py
+++ gtrgtr/gtrgtrapp/views.py
@@ -1,9 +1,10 @@
 from .models import Guitars
 from django.views.generic import ListView
 # Create your views here.
+
+
 class IndexView(ListView):
     model = Guitars
     context_object_name = 'guitars'
     paginate_by = 21
     template_name = 'index.html'
-
