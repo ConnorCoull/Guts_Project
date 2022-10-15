c974f2bd35d65f2cb755ffec389eb1ae023c5671
Alicolliar <alicolliar@gmail.com>
2022-10-15 15:31:46 +0100

    backend: Added the enum dicts and the necessary enricher

--- gtrgtr/preheat-app.py
+++ gtrgtr/preheat-app.py
@@ -10,6 +10,56 @@ auth_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
+bodyShapes = {
+    1: 'SStyle',
+    2: 'TStyle',
+    3: 'DoubleCut',
+    4: 'Offset',
+    5: 'HollowBody',
+    6: 'VStyle',
+    7: 'SmallBody',
+    8: 'Orchestral',
+    9: 'Dreadnought',
+    10: 'Grand Auditorium',
+    11: 'Jumbo',
+    12: 'Explorer',
+    13: 'SingleCut',
+    14: 'Combo',
+    15: 'Head',
+    16: 'Cabinet',
+}
+
+colour = {
+    1: 'Red',
+    2: 'Orange',
+    3: 'Yellow',
+    4: 'Green',
+    5: 'Blue',
+    6: 'Purple',
+    7: 'Pink',
+    8: 'Brown',
+    9: 'Gold',
+    10: 'Silver',
+    11: 'Grey',
+    12: 'Black',
+    13: 'White',
+    14: 'Natural',
+    15: 'Multicolour',
+}
+
+pickup = {
+    1: 'ElectroAcoustic',
+    2: 'SS',
+    3: 'SSS',
+    4: 'HH',
+    5: 'HHH',
+    6: 'HS',
+    7: 'HSS',
+    8: 'HSH',
+    9: 'P90',
+    10: 'S',
+    11: 'H'
+}
 
 
 def getGtrGtrObjects():
@@ -29,7 +79,7 @@ def getSpotifyURL(spotifyID):
     
 def consolidateGtrsAndSongs(gtrsObjects, gtrsSongsObjects):
     consolObjects = []
-    finalIndex = len(gtrsObjects) - 1
+    for gtr in gtrsObjects:
     for i in range(0, finalIndex):
         finalObject = {}
         gtr = gtrsObjects[i]
@@ -53,7 +103,23 @@ def consolidateGtrsAndSongs(gtrsObjects, gtrsSongsObjects):
         consolObjects.append(finalObject)
     return consolObjects
 
-    
+
+def enumEnricher(consolObjects):
+    newObjects = []
+    for guitar in consolObjects:
+        print(guitar)
+        currentBodyShape = guitar['bodyShape']
+        currentColour = guitar['colour']
+        currentPickup = guitar['pickup']
+        guitar['bodyShape'] = bodyShapes[currentBodyShape]
+        guitar['colour'] = bodyShapes[currentColour]
+        guitar['pickup'] = bodyShapes[currentPickup]
+        newObjects.append(guitar)
+        print(guitar)
+    return newObjects
+
+
 if __name__ == "__main__":
     gtrgtrJSON, gtrSongsJSON = getGtrGtrObjects()
     consolidatedJSON = consolidateGtrsAndSongs(gtrgtrJSON, gtrSongsJSON)
+    print(enumEnricher(consolidatedJSON))
