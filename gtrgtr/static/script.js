5a64071b4aa7e9fdf48ed2cb2a8de52aa05b5ae4
Connor <2470724c@student.gla.ac.uk>
2022-10-16 06:48:38 +0100

    SAFE this is the MVP ready for display

--- gtrgtr/static/script.js
+++ gtrgtr/static/script.js
@@ -14,7 +14,6 @@ if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 }
 
 const _openModal = (incrementingIndex) => {
-  console.log("hello there");
   document.getElementById("myModal" + incrementingIndex).style.display =
     "block";
 };
